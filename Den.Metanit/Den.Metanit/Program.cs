29b24ca84ea9a5b8520bd88018266f9f4ade6bd5
Den <den26544@mail.ru>
2020-04-19 06:23:31 +0300

    Start

---
+++ Den.Metanit/Den.Metanit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Den.Metanit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter2.Greeting();
+            Console.ReadKey();
+        }
+    }
+}
